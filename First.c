548382b18b34155a1061a388460c5fe4d3482af6
Dhanaji Salavi <DSalavi21@users.noreply.github.com>
2017-09-22 17:01:22 +0000

    Create First.c

---
+++ First.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+ int flag=0;
+ printf("Parent\n");
+
+  if(fork()==0)
+  {
+	printf("Child\n");
+	flag=1;
+  }
+
+if(flag==0)
+{
+	printf("Wait call by parent\n");
+	wait(10);
+}
+
+	if(flag==1)
+	{
+	  printf("Child ends\n");
+	  exit(0);
+	}
+printf("Parent ends\n");
+
+return 0;
+}
+
+/*	OUTPUT
+
+Parent
+Wait call by parent
+Child
+Child ends
+Parent ends
+
+*/
